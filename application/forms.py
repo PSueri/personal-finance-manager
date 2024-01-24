260c27862f3ed3a1fffcb4f022674688edce4fea
piero <pierosueri@yahoo.it>
2024-01-25 00:02:08 +0100

    Show table history, date in form, delete function

--- application/forms.py
+++ application/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, IntegerField, SubmitField
 from wtforms.validators import DataRequired
+from wtforms.fields import DateField
+from datetime import datetime
 
 class UserInputForm(FlaskForm):
     type = SelectField('Type', validators=[DataRequired()],
@@ -14,6 +16,8 @@ class UserInputForm(FlaskForm):
     second_category = SelectField('Second Category', validators=[DataRequired()],
                                     choices =[('Salary', 'Salary'),
                                                ('Rent', 'Rent')])
+    date = DateField('Date', validators=[DataRequired()],
+                                    format='%Y-%m-%d', default=datetime.now())
 
     amount = IntegerField('Amount', validators=[DataRequired()])
     submit = SubmitField("Add Transaction")
--- application/routes.py
+++ application/routes.py
@@ -14,9 +14,23 @@ def add_transaction():
         entry=TransactionHistory(type=form.type.data,
                                  first_category=form.first_category.data,
                                  second_category=form.second_category.data,
-                                 amount=form.amount.data)
+                                 amount=form.amount.data,
+                                 date=form.date.data)
         db.session.add(entry)
         db.session.commit()
         flash("Successful entry", 'success')
-        return redirect(url_for('index'))
+        return redirect(url_for('show_transactions'))
     return render_template('add.html', title='Add', form=form)
+
+@app.route("/transactions")
+def show_transactions():
+    entries=TransactionHistory.query.order_by(TransactionHistory.date.desc()).all()
+    return render_template('show_transactions.html', title='Transactions', entries=entries)
+
+@app.route("/delete/<int:entry_id>")
+def delete(entry_id):
+    entry = TransactionHistory.query.get_or_404(int(entry_id))
+    db.session.delete(entry)
+    db.session.commit()
+    flash("Successful Deletion", 'success')
+    return redirect(url_for('show_transactions'))
