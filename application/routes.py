d24309b583a592194b9ec15da80263f5bd0260bc
piero <pierosueri@yahoo.it>
2024-01-25 01:30:02 +0100

    First Simple Dashboard

--- application/routes.py
+++ application/routes.py
@@ -2,6 +2,9 @@ from application import app, db
 from flask import render_template, flash, redirect, url_for, get_flashed_messages
 from application.forms import UserInputForm
 from application.models import TransactionHistory
+from flask_sqlalchemy import SQLAlchemy
+import json
+import calendar
 
 @app.route("/")
 def index():
@@ -27,6 +30,31 @@ def show_transactions():
     entries=TransactionHistory.query.order_by(TransactionHistory.date.desc()).all()
     return render_template('show_transactions.html', title='Transactions', entries=entries)
 
+@app.route('/dashboard')
+def dashboard():
+    income_vs_expenses = db.session.query(db.func.sum(TransactionHistory.amount),
+                    TransactionHistory.type).group_by(TransactionHistory.type).order_by(TransactionHistory.type).all()
+
+    dates = db.session.query(db.func.sum(TransactionHistory.amount),
+                             db.func.extract('year',TransactionHistory.date),
+                             db.func.extract('month',TransactionHistory.date)).group_by(
+        db.func.extract('month',TransactionHistory.date),
+        db.func.extract('year',TransactionHistory.date)).order_by(
+        db.func.extract('month',TransactionHistory.date),
+        db.func.extract('year',TransactionHistory.date)).all()
+
+    income_expense=[]
+    for total_amount, _ in income_vs_expenses:
+        income_expense.append(total_amount)
+
+    over_time_expenditure = []
+    dates_label = []
+    for amount, year, month in dates:
+        tmp_label=str(year)+" "+calendar.month_abbr[month]
+        dates_label.append(tmp_label)
+        over_time_expenditure.append(amount)
+    return render_template('dashboard.html', title='Dashboard', income_vs_expenses=json.dumps(income_expense),
+                           over_time_expenditure=json.dumps(over_time_expenditure), dates_label=json.dumps(dates_label))
 @app.route("/delete/<int:entry_id>")
 def delete(entry_id):
     entry = TransactionHistory.query.get_or_404(int(entry_id))
