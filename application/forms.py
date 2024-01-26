07aaefc8c454b17d91c9ff98d55d8b3a79015cb5
piero <pierosueri@yahoo.it>
2024-01-26 01:52:08 +0100

    Dynamic conditional choiches in form

--- application/forms.py
+++ application/forms.py
@@ -4,20 +4,40 @@ from wtforms.validators import DataRequired
 from wtforms.fields import DateField
 from datetime import datetime
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
+from wtforms.fields import DateField
+from datetime import datetime
+
 class UserInputForm(FlaskForm):
-    type = SelectField('Type', validators=[DataRequired()],
-                                    choices =[('Income', 'Income'),
-                                               ('Expense', 'Expense')])
+    TYPES = [('Income', 'Income'), ('Expense', 'Expense')]
 
-    first_category = SelectField('First Category', validators=[DataRequired()],
-                                    choices =[('Work', 'Work'),
-                                               ('Home', 'Home')])
+    FIRST_CATEGORIES = [
+        ('Work', 'Work'), ('OtherIncome', 'OtherIncome'), ('Home', 'Home'),
+        ('Cash', 'Cash'), ('Bills', 'Bills'), ('Food', 'Food'),
+        ('Taxes', 'Taxes'), ('Transportation', 'Transportation'),
+        ('LoanOrSubscription', 'LoanOrSubscription'), ('OtherExpense', 'OtherExpense')
+    ]
 
-    second_category = SelectField('Second Category', validators=[DataRequired()],
-                                    choices =[('Salary', 'Salary'),
-                                               ('Rent', 'Rent')])
-    date = DateField('Date', validators=[DataRequired()],
-                                    format='%Y-%m-%d', default=datetime.now())
+    SECOND_CATEGORIES = [
+        ('Salary', 'Salary'), ('Bonus', 'Bonus'), ('ThirteenthSalary', 'ThirteenthSalary'),
+        ('Gift', 'Gift'), ('OtherIncome', 'OtherIncome'), ('Rent', 'Rent'),
+        ('Grocery', 'Grocery'), ('Cash', 'Cash'), ('Light', 'Light'), ('Fine', 'Fine'),
+        ('OtherBill', 'OtherBill'), ('Restourant', 'Restourant'), ('TakeAway', 'TakeAway'),
+        ('Bar', 'Bar'), ('Bank', 'Bank'), ('OtherTaxes', 'OtherTaxes'),
+        ('CarRent', 'CarRent'), ('PublicTransport', 'PublicTransport'), ('Loan', 'Loan'),
+        ('PSPlus', 'PSPlus'), ('CrunchyRoll', 'CrunchyRoll'), ('FantasyFootball', 'FantasyFootball'),
+        ('Telephone', 'Telephone'), ('Cigarettes', 'Cigarettes'), ('Present', 'Present'), ('Other', 'Other')
+    ]
+
+    type = SelectField('Type', validators=[DataRequired()], choices=[('Select', ' ')] + TYPES)
+
+    first_category = SelectField('First Category', validators=[DataRequired()], choices=[('Select', ' ')] + FIRST_CATEGORIES)
+
+    second_category = SelectField('Second Category', validators=[DataRequired()], choices=[('Select', ' ')] + SECOND_CATEGORIES)
+
+    date = DateField('Date', validators=[DataRequired()], format='%Y-%m-%d', default=datetime.now())
 
     amount = IntegerField('Amount', validators=[DataRequired()])
     submit = SubmitField("Add Transaction")
