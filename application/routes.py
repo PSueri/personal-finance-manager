c10e6e36385ff3d6227a0b0dfcd5005a1fb2ed50
piero <pierosueri@yahoo.it>
2024-06-15 00:58:34 +0200

    code refactoring

--- application/routes.py
+++ application/routes.py
@@ -4,9 +4,10 @@ from application.forms import UserInputForm, SelectYearMonthForm
 from application.models import TransactionHistory
 from flask_sqlalchemy import SQLAlchemy
 import json
+import requests
 import pandas as pd
 import calendar
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 @app.route("/")
@@ -35,24 +36,6 @@ def show_transactions():
 
 @app.route('/dashboard', methods=["GET", "POST"])
 def dashboard():
-    selectionform = SelectYearMonthForm()
-    if selectionform.validate_on_submit():
-        # Assuming the year and month are passed as query parameters from the frontend
-        year = selectionform.selected_year.data
-        selmonth = selectionform.selected_month.data
-    else:
-        year=0
-        selmonth=0
-    # Set default values if not provided
-    if not int(year):
-        print('not year')
-        year = datetime.now().year
-    if not int(selmonth):
-        print('not month')
-        selmonth = datetime.now().month
-    print(selmonth)
-    current_period = str(year)+', '+calendar.month_name[int(selmonth)]
-
     def get_date_range_df(format_string="%Y %b"):
         """
         This function generates an ordered list of dates in the specified format
@@ -76,9 +59,28 @@ def dashboard():
             one_year_ago = one_year_ago + relativedelta(months=1)
             dates_df = pd.DataFrame(dates, columns=['yearmonth'])
         return dates_df
+    # Get selected period
+    selectionform = SelectYearMonthForm()
+    if selectionform.validate_on_submit():
+        year = selectionform.selected_year.data
+        selmonth = selectionform.selected_month.data
+    else:
+        year = 0
+        selmonth = 0
+    # Set default values if not provided
+    if not int(year):
+        print('not year')
+        year = datetime.now().year
+    if not int(selmonth):
+        print('not month')
+        selmonth = datetime.now().month
+
+    current_period = str(year)+', '+calendar.month_name[int(selmonth)]
+    print(current_period)
 
     one_year_dates = get_date_range_df()
     print(one_year_dates)
+
     income_dates = db.session.query(db.func.sum(TransactionHistory.amount),
                                           TransactionHistory.date).filter_by(type='Income').group_by(
                                             TransactionHistory.date).order_by(
@@ -100,32 +102,30 @@ def dashboard():
                                             TransactionHistory.second_category).order_by(
                                             TransactionHistory.second_category).all()
 
-    # Income DataFrame
-    income=[]
-    yearmonth=[]
-    for amount, date in income_dates:
-        income.append(amount)
-        yearmonth.append(str(date.year)+" "+calendar.month_abbr[date.month])
-    dict={'income':income, 'yearmonth':yearmonth}
-    income_df=pd.DataFrame(data=dict)
-    income_gouped=income_df.groupby("yearmonth", as_index=False).sum()
+    income_dict = {
+        "income": [amount for amount, _ in income_dates],
+        "yearmonth": [f"{date.year} {calendar.month_abbr[date.month]}" for _, date in income_dates]
+    }
 
-    # Expense DataFrame
-    expense=[]
-    yearmonth=[]
-    for amount, date in expense_dates:
-        expense.append(amount)
-        yearmonth.append(str(date.year)+" "+calendar.month_abbr[date.month])
-    dict={'expense':expense, 'yearmonth':yearmonth}
-    expense_df=pd.DataFrame(data=dict)
-    expense_gouped=expense_df.groupby("yearmonth", as_index=False).sum()
+    expense_dict = {
+        "expense": [amount for amount, _ in expense_dates],
+        "yearmonth": [f"{date.year} {calendar.month_abbr[date.month]}" for _, date in expense_dates]
+    }
+
+    # Create DataFrames for income and expenses
+    income_df = pd.DataFrame(income_dict)
+    expense_df = pd.DataFrame(expense_dict)
+
+    # Group and sum using DataFrame methods
+    income_grouped = income_df.groupby("yearmonth", as_index=False).sum()
+    expense_grouped = expense_df.groupby("yearmonth", as_index=False).sum()
 
     # join df
-    income_one_year = one_year_dates.merge(income_gouped, on='yearmonth', how='left').fillna(0)
-    income_expense_dates_df=income_one_year.merge(expense_gouped, on='yearmonth', how='left').fillna(0)
-    income_expense_dates_df["netflow"]=income_expense_dates_df["income"]-income_expense_dates_df["expense"]
-    income_expense_dates_df["month"]=pd.to_datetime(income_expense_dates_df["yearmonth"].str[-3:], format='%b').dt.month
-    income_expense_dates_df["year"]=pd.to_datetime(income_expense_dates_df["yearmonth"].str[:4], format='%Y').dt.year
+    income_expense_dates_df = (one_year_dates.merge(income_grouped, on='yearmonth', how='left').fillna(0)
+                               .merge(expense_grouped, on='yearmonth', how='left').fillna(0))
+    income_expense_dates_df["netflow"] = income_expense_dates_df["income"] - income_expense_dates_df["expense"]
+    income_expense_dates_df["month"] = pd.to_datetime(income_expense_dates_df["yearmonth"], format='%Y %b').dt.month
+    income_expense_dates_df["year"] = pd.to_datetime(income_expense_dates_df["yearmonth"], format='%Y %b').dt.year
     income_expense_dates_df=income_expense_dates_df.sort_values(by=['year', 'month'], ascending=True)
     print(income_expense_dates_df)
     # netflow
@@ -137,17 +137,12 @@ def dashboard():
     # expenses
     expense_month = income_expense_dates_df['expense'].tolist()
 
-    cat_exp_amount = []
-    cat_exp_label = []
-    for amount, category in category_expenses:
-        cat_exp_label.append(category)
-        cat_exp_amount.append(amount)
+    # Create lists using list comprehension for monthly category amounts
+    cat_exp_label = [category for _, category in category_expenses]
+    cat_exp_amount = [amount for amount, _ in category_expenses]
 
-    cat_inc_amount = []
-    cat_inc_label = []
-    for amount, category in category_incomes:
-        cat_inc_label.append(category)
-        cat_inc_amount.append(amount)
+    cat_inc_label = [category for _, category in category_incomes]
+    cat_inc_amount = [amount for amount, _ in category_incomes]
 
     return render_template('dashboard.html', title='Dashboard',
                            income_month=json.dumps(income_month),
@@ -167,9 +162,3 @@ def delete(entry_id):
     db.session.commit()
     flash("Successful Deletion", 'success')
     return redirect(url_for('show_transactions'))
-
-
-@app.route("/crypto")
-def show_crypto_prices():
-    entries=TransactionHistory.query.order_by(TransactionHistory.date.desc()).all()
-    return render_template('cryptocurrencies.html', title='Cryptocurrencies', entries=entries)
